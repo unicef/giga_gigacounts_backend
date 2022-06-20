953a253752065e123c3bf951aba0920d76b527d5
Alexandre Ramos <alexandre.rsan@gmail.com>
2022-06-20 08:54:06 -0300

    env var validation fixed by env

--- env.ts
+++ env.ts
@@ -1,27 +1,40 @@
-/*
-|--------------------------------------------------------------------------
-| Validating Environment Variables
-|--------------------------------------------------------------------------
-|
-| In this file we define the rules for validating environment variables.
-| By performing validation we ensure that your application is running in
-| a stable environment with correct configuration values.
-|
-| This file is read automatically by the framework during the boot lifecycle
-| and hence do not rename or move this file to a different location.
-|
-*/
-
 import Env from '@ioc:Adonis/Core/Env'
 
-export default Env.rules({
-  HOST: Env.schema.string({ format: 'host' }),
-  PORT: Env.schema.number(),
-  APP_KEY: Env.schema.string(),
-  APP_NAME: Env.schema.string(),
-  DRIVE_DISK: Env.schema.enum(['local'] as const),
-  NODE_ENV: Env.schema.enum(['dev', 'production', 'test'] as const),
-  AZURE_STORAGE_CONNECTION_STRING: Env.schema.string(),
-  AZURE_CONTAINER_NAME: Env.schema.string(),
-  ORIGINS_ALLOWED: Env.schema.string(),
-})
+interface EnvInterface {
+  HOST: string
+  PORT: number
+  APP_KEY: string
+  APP_NAME: string
+  DRIVE_DISK: 'local'
+  NODE_ENV: 'dev' | 'production' | 'test'
+  AZURE_STORAGE_CONNECTION_STRING?: string
+  AZURE_CONTAINER_NAME?: string
+  ORIGINS_ALLOWED?: string
+}
+
+let rules: EnvInterface
+
+if (process.env.production) {
+  rules = Env.rules({
+    HOST: Env.schema.string({ format: 'host' }),
+    PORT: Env.schema.number(),
+    APP_KEY: Env.schema.string(),
+    APP_NAME: Env.schema.string(),
+    DRIVE_DISK: Env.schema.enum(['local'] as const),
+    NODE_ENV: Env.schema.enum(['dev', 'production', 'test'] as const),
+    AZURE_STORAGE_CONNECTION_STRING: Env.schema.string(),
+    AZURE_CONTAINER_NAME: Env.schema.string(),
+    ORIGINS_ALLOWED: Env.schema.string(),
+  })
+} else {
+  rules = Env.rules({
+    HOST: Env.schema.string({ format: 'host' }),
+    PORT: Env.schema.number(),
+    APP_KEY: Env.schema.string(),
+    APP_NAME: Env.schema.string(),
+    DRIVE_DISK: Env.schema.enum(['local'] as const),
+    NODE_ENV: Env.schema.enum(['dev', 'production', 'test'] as const),
+  })
+}
+
+export default rules
