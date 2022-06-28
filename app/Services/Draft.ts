4b7733d9b9a1c7b1444a27271b04662983cc2a26
Alexandre Ramos <alexandre.rsan@gmail.com>
2022-06-28 10:12:45 -0300

    update draft bug fixed

--- app/Services/Draft.ts
+++ app/Services/Draft.ts
@@ -3,8 +3,27 @@ import Attachment from 'App/Models/Attachment'
 import Draft from 'App/Models/Draft'
 import Metric from 'App/Models/Metric'
 import School from 'App/Models/School'
+import { DateTime } from 'luxon'
 
 import dto from 'App/DTOs/Draft'
+
+export interface UpdateDraft {
+  id: number
+  name?: string
+  countryId?: number
+  governmentBehalf?: boolean
+  ltaId?: number
+  currencyId?: number
+  budget?: string
+  frequencyId?: number
+  startDate?: string
+  endDate?: string
+  ispId?: number
+  createdBy?: number
+  attachments?: { attachments: { id: number }[] }
+  schools?: { schools: { id: number }[] }
+  expectedMetrics?: { metrics: { metricId: number; value: number }[] }
+}
 
 const getDraft = async (draftId: number) => {
   const draft = await Draft.find(draftId)
@@ -43,7 +62,7 @@ const destructDraftsArray = (object?: { id: number }[]) => {
   return (object || []).map((x) => x.id)
 }
 
-const updateDraft = async (draftData: Draft): Promise<Draft> => {
+const updateDraft = async (draftData: UpdateDraft): Promise<Draft> => {
   const draft = await Draft.find(draftData.id)
 
   if (!draft) throw new NotFoundException('Draft not found', 404, 'NOT_FOUND')
@@ -55,8 +74,12 @@ const updateDraft = async (draftData: Draft): Promise<Draft> => {
   draft.currencyId = draftData?.currencyId
   draft.budget = draftData?.budget
   draft.frequencyId = draftData?.frequencyId
-  draft.startDate = draftData?.startDate
-  draft.endDate = draftData?.endDate
+  draft.startDate = draftData.startDate
+    ? DateTime.fromFormat(draftData.startDate, 'yyyy-MM-dd')
+    : undefined
+  draft.endDate = draftData.endDate
+    ? DateTime.fromFormat(draftData.endDate, 'yyyy-MM-dd').set({ hour: 23, minute: 59, second: 59 })
+    : undefined
   draft.ispId = draftData?.ispId
   draft.createdBy = draftData?.createdBy
   draft.attachments = draftData?.attachments
