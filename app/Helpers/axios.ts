bce76adba71eceb7beca097c86227eb256ba88d3
Alexandre Ramos <alexandre.rsan@gmail.com>
2022-07-19 09:26:49 -0300

    Load countries from unicef done

---
+++ app/Helpers/axios.ts
@@ -0,0 +1,12 @@
+import axios, { AxiosRequestHeaders } from 'axios'
+
+const createInstance = (baseUrl: string, headers: AxiosRequestHeaders) => {
+  return axios.create({
+    baseURL: baseUrl,
+    headers,
+  })
+}
+
+export default {
+  createInstance,
+}
