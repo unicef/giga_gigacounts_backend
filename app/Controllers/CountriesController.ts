e91c85c1881b34c57433504ccfc40e5021361772
Alexandre Ramos <alexandre.rsan@gmail.com>
2022-07-29 15:32:57 -0300

    filter countries done

--- app/Controllers/CountriesController.ts
+++ app/Controllers/CountriesController.ts
@@ -3,8 +3,9 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import service from 'App/Services/Country'
 
 export default class CountriesController {
-  public async listCountries({ response }: HttpContextContract) {
-    const countries = await service.listCountries()
+  public async listCountries({ response, auth }: HttpContextContract) {
+    if (!auth.user) return
+    const countries = await service.listCountries(auth.user)
     return response.ok(countries)
   }
 }
