cca2f34aec14ba1391470530a7698e59c4d7ba4b
Alexandre Ramos <alexandre.rsan@gmail.com>
2022-09-14 15:58:54 -0300

    list lta by role country

--- app/Controllers/LtasController.ts
+++ app/Controllers/LtasController.ts
@@ -3,9 +3,10 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import service from 'App/Services/Lta'
 
 export default class LtasController {
-  public async listLtas({ response, request }: HttpContextContract) {
+  public async listLtas({ response, request, auth }: HttpContextContract) {
+    if (!auth.user) return
     const { countryId } = request.qs()
-    const ltas = await service.listLtas(countryId)
+    const ltas = await service.listLtas(auth.user, countryId)
     return response.ok(ltas)
   }
 }
