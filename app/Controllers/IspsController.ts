b0a1661974efe23fb4847c9853b9c0ed742ea923
Alexandre Ramos <alexandre.rsan@gmail.com>
2022-09-15 10:02:51 -0300

    list isp by user role country

--- app/Controllers/IspsController.ts
+++ app/Controllers/IspsController.ts
@@ -3,9 +3,10 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import service from 'App/Services/Isp'
 
 export default class IspsController {
-  public async listIsps({ response, request }: HttpContextContract) {
+  public async listIsps({ response, request, auth }: HttpContextContract) {
+    if (!auth.user) return
     const { countryId, ltaId } = request.qs()
-    const isps = await service.listIsps(ltaId, countryId)
+    const isps = await service.listIsps(auth.user, ltaId, countryId)
     return response.ok(isps)
   }
 }
--- app/Services/Isp.ts
+++ app/Services/Isp.ts
@@ -1,7 +1,14 @@
 import Isp from 'App/Models/Isp'
+import User from 'App/Models/User'
 
-const listIsps = async (ltaId?: number, countryId?: number): Promise<Isp[]> => {
+import userService from 'App/Services/User'
+import { roles } from 'App/Helpers/constants'
+
+const listIsps = async (user: User, ltaId?: number, countryId?: number): Promise<Isp[]> => {
   const query = Isp.query()
+  if (!userService.checkUserRole(user, [roles.gigaAdmin])) {
+    countryId = user.countryId
+  }
   if (countryId) query.where('country_id', countryId)
   if (ltaId) {
     query.whereHas('ltas', (qry) => {
